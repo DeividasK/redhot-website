60e4e6a5336e8c2dd2e44d165ced69097f109357
DeividasK <deividas.karzinauskas@gmail.com>
2014-11-03 16:26:00 +0200

    Version #5
    Added responsiveness

--- js/hide-navigation.js
+++ js/hide-navigation.js
@@ -1,14 +1,27 @@
 jQuery(document).ready(function () {
-	jQuery('.navbar').addClass('hideContent');			
+	jQuery('.navbar').addClass('hideContent');
+	if ($(window).width() < 768) {
+		jQuery('.navbar-nav>li>a').removeClass('white');
+	}
 	jQuery(window).scroll(function () {
-		if (jQuery(window).scrollTop() > $(window).height()-1) {
-			jQuery('#back-to-top').removeClass('hide');
-			jQuery('.navbar-brand').removeClass('hide');
-			jQuery('.navbar-nav>li>a').removeClass('white');
+		 if ($(window).width() > 768) {
+			if (jQuery(window).scrollTop() > $(window).height()-70) {
+				jQuery('#back-to-top').removeClass('hide');
+				jQuery('.navbar-brand').removeClass('hide');
+				jQuery('.navbar-nav>li>a').removeClass('white');
+			} else {
+				jQuery('#back-to-top').addClass('hide');
+				jQuery('.navbar-brand').addClass('hide');
+				jQuery('.navbar-nav>li>a').addClass('white');
+			}
 		} else {
-			jQuery('#back-to-top').addClass('hide');
-			jQuery('.navbar-brand').addClass('hide');
-			jQuery('.navbar-nav>li>a').addClass('white');
+			if (jQuery(window).scrollTop() > $(window).height()-70 && $(window).width() < 768) {
+				jQuery('#back-to-top').removeClass('hide');
+				jQuery('.navbar-brand').removeClass('hide');
+			} else {
+				jQuery('#back-to-top').addClass('hide');
+				jQuery('.navbar-brand').addClass('hide');
+			}
 		}
 	});
 });
