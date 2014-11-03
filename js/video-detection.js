18009a88bb3486d1a4cf622f7a87e1bf8b3e656e
DeividasK <deividas.karzinauskas@gmail.com>
2014-11-03 11:59:57 +0200

    Version #4
    Added carousel.
    Added jquery load.

--- js/video-detection.js
+++ js/video-detection.js
@@ -1,27 +1,31 @@
-        $(document).ready(function() {
-            // Get media - with autoplay disabled (audio or video)
-            var media = $('video').not("[autoplay='autoplay']");
-            var tolerancePixel = 250;
-            var videoStatus = 0;
+$(document).ready(function() {
+    // Get media - with autoplay disabled (audio or video)
+    var media = $('video').not("[autoplay='autoplay']");
+    var tolerancePixel = 250;
+    var videoStatus = 0;
 
-            function checkMedia(){
-                // Get current browser top and bottom
-                var scrollTop = $(window).scrollTop() + tolerancePixel;
-                var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+    function checkMedia(){
+        // Get current browser top and bottom
+        var scrollTop = $(window).scrollTop() + tolerancePixel;
+        var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
 
-                //if ($(window).scrollTop() > $(window).height() - 10) {
-                media.each(function(index, el) {
-                    var yTopMedia = $(this).offset().top;
-                    var yBottomMedia = $(this).height() + yTopMedia;
-                    
+        //if ($(window).scrollTop() > $(window).height() - 10) {
+        media.each(function(index, el) {
+            var yTopMedia = $(this).offset().top;
+            var yBottomMedia = $(this).height() + yTopMedia;
+            
 
-                    if(scrollTop < yBottomMedia && scrollBottom > yTopMedia && videoStatus == 0){
-                        $(this).get(0).play();
-                        videoStatus = 1;
-                    }
-                });
-
-                //}
+            if(scrollTop < yBottomMedia && scrollBottom > yTopMedia && videoStatus == 0){
+                $(this).get(0).play();
+                videoStatus = 1;
             }
-            $(document).on('scroll', checkMedia);
         });
+
+        //}
+    }
+    $(document).on('scroll', checkMedia);
+});
+
+$('.carousel').carousel({
+    interval: false
+}) 
