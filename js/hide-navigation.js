b5304ee399bfec28287c1fed35ae4845682d6164
DeividasK <deividas.karzinauskas@gmail.com>
2014-11-04 11:19:11 +0200

    #8.1 - Video height fix

--- js/hide-navigation.js
+++ js/hide-navigation.js
@@ -9,10 +9,12 @@ jQuery(document).ready(function () {
 				jQuery('#back-to-top').removeClass('hide');
 				jQuery('.navbar-brand').removeClass('hide');
 				jQuery('.navbar-nav>li>a').removeClass('white');
+				jQuery('.nav.navbar-nav.navbar-right').addClass('nav-background');
 			} else {
 				jQuery('#back-to-top').addClass('hide');
 				jQuery('.navbar-brand').addClass('hide');
 				jQuery('.navbar-nav>li>a').addClass('white');
+				jQuery('.nav.navbar-nav.navbar-right').removeClass('nav-background');
 			}
 		} else {
 			if (jQuery(window).scrollTop() > $(window).height()-70 && $(window).width() < 768) {
